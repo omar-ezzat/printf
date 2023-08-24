6528f66b81f8534580cc0517ee0671db1b81b869
Rana5Ahmed <ranaahmedroka@gmail.com>
2023-08-24 08:30:37 -0700

    Number.c

--- number.c
+++ number.c
@@ -41,15 +41,15 @@ return(ptr);
 */
 int print_unsigned (va_list ap, params_t *params)
 {
-unsigned long 1;
-if (params->1_modifier)
-1 = (unsigned long) va_arg (ap, unsigned long);
+unsigned long l;
+if (params->l_modifier)
+l = (unsigned long) va_arg (ap, unsigned long);
 else if (params->h_modifier)
-1 = (unsigned short int) va_arg (ap, unsigned int);
+l = (unsigned short int) va_arg (ap, unsigned int);
 else
-1 = (unsigned int) va_arg (ap, unsigned int);
+l = (unsigned int) va_arg (ap, unsigned int);
 params->unsign = 1;
-return (print_number (convert (1, 10, CONVERT_UNSIGNED, params), params));
+return (print_number (convert (l, 10, CONVERT_UNSIGNED, params), params));
 }
 /**
 *print_address - prints address
